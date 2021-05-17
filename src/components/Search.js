3f3847994decab06accecd80bd90e638e9a9d0d9
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-17 11:02:34 +0530

    setup search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+
+    const [term, setTerm] = useState('');
+
     return (
-        <h1>Search</h1>
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        className="input"
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
 
