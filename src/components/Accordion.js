249eb46c9d708239c6b31082cb05fad2a9aa6714
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-17 10:40:33 +0530

    added functionality to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,23 +2,26 @@ import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
 
-    const [activeIndex, setActiveIndex] = useState(null);
+    const [activeIndex, setActiveIndex] = useState(null);  // array destructuring
 
     const onTitleClicked = (index) => {
         setActiveIndex(index);
     }
 
     const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : '';
+
         return (
             <React.Fragment key={item.title}>
                 <div
-                    className="title active"
+                    className={`title ${active}`}
                     onClick={() => onTitleClicked(index)}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -28,7 +31,6 @@ const Accordion = ({ items }) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
-            <h1>{activeIndex}</h1>
         </div>
     );
 }
