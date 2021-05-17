14c97810338781f4f81271cd1e11926712f7dd7b
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-17 11:28:01 +0530

    setting up axios to fetch data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
     const [term, setTerm] = useState('');
 
+    //cant make the useEffect arrow function async, therefore need to write async function inside it
     useEffect(() => {
-        console.log()
+
+        const search = async () => {
+            const response = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+
+            console.log(response);
+        };
+
+        search();
+
     }, [term]);
 
     return (
