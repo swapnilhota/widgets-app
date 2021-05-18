e21760d98807495c0e7320ebc26dc806d0ad3767
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-18 10:02:14 +0530

    dont show selected color in list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,6 +3,11 @@ import React from 'react';
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const renderedOptions = options.map((option) => {
+
+        if (option.value === selected.value) {
+            return null;
+        }
+
         return (
             <div key={option.value} onClick={() => onSelectedChange(option)} className="item">
                 {option.label}
