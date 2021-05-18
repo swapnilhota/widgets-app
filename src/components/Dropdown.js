6864104484abb886c51791ffacab10b995945b70
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-18 10:35:26 +0530

    used useRef to close dropdown whenever user clicks outside of it

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        })
+    }, []);
 
     const renderedOptions = options.map((option) => {
 
@@ -18,7 +30,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     })
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div
