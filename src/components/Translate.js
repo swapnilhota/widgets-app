a10264858bc730d3a7d02b89c910f5174fa46cd1
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-19 10:58:28 +0530

    created convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,10 @@
+// GoogleTranslateApi = AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+/* This API can only be used when your browser is at ‘http://localhost:3000’. 
+ If you try to make a request to the API from any other address, the request will fail. */
+
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     {
@@ -26,10 +31,13 @@ const Translate = () => {
             <div className="ui form">
                 <div className="field">
                     <label className="label">Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
                 </div>
             </div>
-            <input value={text} onChange={(e) => setText(e.target.value)} />
             <Dropdown label="Select a Language" selected={language} onSelectedChange={setLanguage} options={options} />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert language={language} text={text} />
         </div>
     );
 };
