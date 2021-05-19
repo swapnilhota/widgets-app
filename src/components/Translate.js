08f24f8fe622e27f82856cc3699c2cd5143fc574
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-19 10:37:01 +0530

    created translate component

---
+++ src/components/Translate.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    },
+    {
+        label: 'Arabic',
+        value: 'ar'
+    },
+    {
+        label: 'Hindi',
+        value: 'hi'
+    }
+];
+
+const Translate = () => {
+    return (
+        <div>
+            <Dropdown />
+        </div>
+    );
+};
+
+export default Translate;
