1053455e7471af07618d2fc4885e6b3b75a0b7f9
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-20 10:32:09 +0530

    update Link.js

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,7 +4,11 @@ const Link = ({ className, href, children }) => {
 
     const onClick = (event) => {
         event.preventDefault();
-        window.history.pushState({}, '', href);
+        window.history.pushState({}, '', href); //changes just url to given href without reloading page
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+        // this will communicate to route that url has changed
     };
 
     return (
