36e0efff2e4ce047706d4d5dae2a9aa54b277acc
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-20 10:35:11 +0530

    detect url change in route component

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+
 const Route = ({ path, children }) => {
+
+    useEffect(() => {
+
+        const onLocationChange = () => {
+
+        }
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        }
+    }, []);
+
     return (
         window.location.pathname === path ? children : null
     );
