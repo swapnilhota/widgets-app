29a2a55be0d1c50d226d26662b23bee6b25c05c5
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-20 09:58:28 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
     {
@@ -34,11 +35,46 @@ const options = [
     }
 ];
 
+const showAccordion = () => {
+    if (window.location.pathname === '/') {
+        return <Accordion items={items} />
+    }
+}
+
+const showList = () => {
+    if (window.location.pathname === '/list') {
+        return <Search />
+    }
+}
+
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+        return <Dropdown />
+    }
+}
+
+const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+        return <Translate />
+    }
+}
+
 const App = () => {
 
     return (
         <div className="ui container">
-            <Translate />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="dropdown">
+                <Dropdown />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 }
